daa6d7cd06452e2529bacf876cb156f4e9eb60f3
Alessandro Lepri <alex.lepri84@gmail.com>
2019-07-01 15:54:32 +0100

    Added contact page

--- src/components/matte/Info.js
+++ src/components/matte/Info.js
@@ -5,10 +5,58 @@ import React from 'react'
 
 const Info = () => (
 
-  <div className="wsite-multicol">
-    <div className="wsite-multicol-table-wrap">
+  <div>
+    <div className="wsite-multicol">
+      <div className="wsite-multicol-table-wrap">
+        <table className="wsite-multicol-table">
+          <tbody className="wsite-multicol-tbody">
+            <tr className="wsite-multicol-tr">
+              <td className="wsite-multicol-col1">
+                <h2 className="wsite-content-title">
+                CONTACT ME
+                </h2>
+                <div className="paragraph1">
+                  <span>What ever your question or query please feel free to get in touch and I will get straight back to   you</span>
+                </div>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+
+    <div className="wsite-multicol">
+      <div className="wsite-multicol-table-wrap">
+        <table className="wsite-multicol-table">
+          <tbody className="wsite-multicol-tbody">
+            <tr className="wsite-multicol-tr">
+              <td className="wsite-multicol-col">
+                <h2 className="wsite-content-title">
+              TELEPHONE
+                </h2>
+                <div className="paragraph1">
+                  <a href="tel:+44123456789">+44 123456789</a>
+                </div>
+              </td>
+
+              <td className="wsite-multicol-col2">
+                <h2 className="wsite-content-title">
+              EMAIL
+                </h2>
+                <div className="paragraph1">
+                  <a href="mailto:test@test.com">test@test.com</a>
+                </div>
+              </td>
+
+
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
   </div>
+
+
 )
 
 export default Info
