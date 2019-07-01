cf7fe5625bb34f1f81dfa91966cfdfb9a441ae47
Alessandro Lepri <alex.lepri84@gmail.com>
2019-07-01 14:20:37 +0100

    Changed setting pages

--- src/components/common/NavBar.js
+++ src/components/common/NavBar.js
@@ -16,6 +16,9 @@ class Navbar extends React.Component{
   handleItemClick(e, { name }){
     this.setState({ activeItem: name })
     if(name === 'home')this.props.history.push('/')
+    if(name === 'aboutme')this.props.history.push('/aboutme')
+    if(name === 'service')this.props.history.push('/service')
+    if(name === 'contact')this.props.history.push('/')
   }
 
 
@@ -28,29 +31,29 @@ class Navbar extends React.Component{
         <Menu stackable inverted pointing secondary>
           <Menu.Item
             name='home'
-            active={this.props.location.pathname === '/aboutme'}
+            active={this.props.location.pathname === '/'}
             onClick={this.handleItemClick}
           > <Icon name='home' /> Home
           </Menu.Item>
 
           <Menu.Menu position='right'>
             <Menu.Item
-              name='about me'
-              active={this.props.location.pathname === '/'}
+              name='aboutme'
+              active={this.props.location.pathname === '/aboutme'}
               onClick={this.handleItemClick}
             > <Icon name='globe' /> About Me
             </Menu.Item>
 
             <Menu.Item
               name='service'
-              active={this.props.location.pathname === '/'}
+              active={this.props.location.pathname === '/service'}
               onClick={this.handleItemClick}
             > <Icon name='th' /> Service
             </Menu.Item>
 
             <Menu.Item
               name='contact'
-              active={this.props.location.pathname === '/'}
+              active={this.props.location.pathname === '/contact'}
               onClick={this.handleItemClick}
             > <Icon name='envelope' /> Contact
             </Menu.Item>
--- src/components/matte/AboutMe.js
+++ src/components/matte/AboutMe.js
@@ -1,55 +1,17 @@
 import React from 'react'
 import { Image, Icon } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
-
-import Details from './Details'
-import Info from './Info'
+// import Details from './Details'
+// import Info from './Info'
 
 const AboutMe = () => (
-  <div>
-    <div className="goback">
-      <Link to="/">
-        <h6> Indietro </h6>
-        <Icon name="arrow alternate circle left outline" />
-      </Link>
-    </div>
 
-    <h1 > Contattami </h1>
+  <div className="det">
 
-    <Details />
-    <div className="det">
-    </div>
     <div className="photo">
-      <Image src='images/foto.jpeg' size='large' circular />
-      <Icon name="arrow down" size="large" />
-
+      <Image src='images/foto.jpeg' circular />
     </div>
-
-    <div className="me">
-      <Info />
-    </div>
-
-    <div className="goback">
-      <Link to="/">
-        <h6> Indietro </h6>
-        <Icon name="arrow alternate circle left outline" />
-      </Link>
-    </div>
-
-    <footer>
-      <div className="footer">
-        <div id="copy">
-          <p>&copy; 2019 | Matteo Atti, Personal Trainer | Cento (FE), Italy</p>
-        </div>
-
-        <div id="powered">
-          <p> Powered by Alessandro Lepri </p>
-        </div>
-
-      </div>
-    </footer>
-
 
   </div>
 
