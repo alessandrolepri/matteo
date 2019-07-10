636d8c5ecbe2d8108967925eadd8228a94dbec1f
Alessandro Lepri <alex.lepri84@gmail.com>
2019-07-10 20:49:12 +0100

    Changed write to us

--- src/components/matte/Details.js
+++ src/components/matte/Details.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import { Icon, Step } from 'semantic-ui-react'
-import { MDBRow, MDBCol, MDBCard, MDBCardBody, MDBIcon, MDBBtn, MDBInput } from 'mdbreact'
+import { MDBRow, MDBCol, MDBIcon, MDBBtn, MDBInput } from 'mdbreact'
+
 
 const Details = () => {
   return (
@@ -14,58 +15,44 @@ const Details = () => {
         totam voluptas nostrum quisquam eum porro a pariatur veniam.
       </p>
       <MDBRow>
-        <MDBCol lg="5" className="lg-0 mb-4">
-          <MDBCard>
-            <MDBCardBody>
-              <div className="form-header blue accent-1">
-                <h3 className="mt-2">
-                  <MDBIcon icon="envelope" /> Write to us:
-                </h3>
-              </div>
-              <p className="dark-grey-text">
-                We'll write rarely, but only the best content.
-              </p>
-              <div className="md-form">
-                <MDBInput
-                  icon="user"
-                  label="Your name"
-                  iconClass="grey-text"
-                  type="text"
-                  id="form-name"
-                />
-              </div>
-              <div className="md-form">
-                <MDBInput
-                  icon="envelope"
-                  label="Your email"
-                  iconClass="grey-text"
-                  type="text"
-                  id="form-email"
-                />
-              </div>
-              <div className="md-form">
-                <MDBInput
-                  icon="tag"
-                  label="Subject"
-                  iconClass="grey-text"
-                  type="text"
-                  id="form-subject"
-                />
-              </div>
-              <div className="md-form">
-                <MDBInput
-                  icon="pencil-alt"
-                  label="Icon Prefix"
-                  iconClass="grey-text"
-                  type="textarea"
-                  id="form-text"
-                />
-              </div>
-              <div className="text-center">
-                <MDBBtn color="light-blue">Submit</MDBBtn>
-              </div>
-            </MDBCardBody>
-          </MDBCard>
+        <MDBCol md="6">
+          <form>
+            <p className="h5 text-center mb-4">Write to us</p>
+            <div className="grey-text">
+              <MDBInput
+                label="Your name"
+                icon="user"
+                group type="text"
+                validate error="wrong"
+                success="right"
+              />
+              <MDBInput
+                label="Your email"
+                icon="envelope"
+                group type="email"
+                validate error="wrong"
+                success="right"
+              />
+              <MDBInput
+                label="Subject"
+                icon="tag"
+                group type="text"
+                validate error="wrong"
+                success="right"
+              />
+              <MDBInput
+                label="Your message"
+                type="textarea"
+                rows="8"
+                icon="pencil-alt"
+              />
+            </div>
+            <div className="text-center">
+              <MDBBtn outline color="secondary">
+                      Send <MDBIcon far icon="paper-plane" className="ml-1" />
+              </MDBBtn>
+            </div>
+          </form>
         </MDBCol>
         <MDBCol lg="7">
           <div
